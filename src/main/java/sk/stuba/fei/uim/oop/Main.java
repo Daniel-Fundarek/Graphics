1a7be32afd1ce01d79d13b566214e445213936f3
Daniel-Fundarek <xfundarekd@stuba.sk>
2021-03-23 15:11:29 +0100

    grid layout

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,16 +1,23 @@
 package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame okno = new JFrame();
         okno.setVisible(true);
         okno.setSize(300, 300);
+
         JPanel panel = new JPanel();
         okno.add(panel);
+
+        var panelLayout = new GridLayout(4,4);
+        panel.setLayout(panelLayout);
+
         JLabel label = new JLabel("Hello world");
         panel.add(label);
+
         for (int i = 0;i<10 ; i++){
             panel.add(new JLabel("" + i));
         }
