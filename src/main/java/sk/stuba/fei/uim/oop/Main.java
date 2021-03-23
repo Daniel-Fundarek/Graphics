a01909f3be0934f16358aec694d9b31e9e20f484
Daniel-Fundarek <xfundarekd@stuba.sk>
2021-03-23 14:57:22 +0100

    label

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,15 @@
 package sk.stuba.fei.uim.oop;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame okno = new JFrame();
+        okno.setVisible(true);
+        okno.setSize(300, 300);
+        JLabel label = new JLabel("Hello world");
+        okno.add(label);
+
 
     }
 }
