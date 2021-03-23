c2e3fb3fbdf8ebf181642fe3ea9428fe9c8e94ca
Daniel-Fundarek <xfundarekd@stuba.sk>
2021-03-23 16:14:45 +0100

    grafics for

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -9,35 +9,6 @@ public class Main {
         okno.setVisible(true);
         okno.setSize(300, 300);
 
-        JPanel panel = new JPanel();
-        okno.add(panel);
-
-        var panelLayout = new GridLayout(4,4);
-        panel.setLayout(panelLayout);
-
-        JLabel label = new JLabel("Hello world");
-        panel.add(label);
-        panel.setBackground(Color.BLUE);
-        var panel2 = new JPanel();
-        panel2.setLayout(new BorderLayout());
-
-        panel2.add(new JLabel("top"), BorderLayout.PAGE_START);
-        panel2.add(new JLabel("bottom"), BorderLayout.PAGE_END);
-        panel2.add(new JLabel("left"), BorderLayout.LINE_START);
-        panel2.add(new JLabel("right"), BorderLayout.LINE_END);
-        panel2.add(new JLabel("center"), BorderLayout.CENTER);
-      /*  for(int i = 0; i<5; i++){
-            panel2.add(new JLabel("a"+ i));
-
-        }*/
-
-        for (int i = 0;i<12 ; i++){
-            if(i == 8){
-                panel.add(panel2);
-            }
-            else {
-                panel.add(new JLabel("" + i));
-            }
-        }
+        okno.add(new MyCanvas());
     }
 }
